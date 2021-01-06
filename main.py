b01ae790d8ffbd27f0a425c87a972d909606cc2e
Harsh3305 <harshverma3305@gmail.com>
2021-01-06 19:41:53 +0530

    Minor errors are fixed now

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from moviepy.editor import *
 numberOfVideo = int(input("Please enter number of video: "))
 list_of_video = []
 
-for i in range(1, numberOfVideo):
+for i in range(0, numberOfVideo):
     name = input("Please enter path of video number " + str(i) + ": ")
     clip = VideoFileClip(name)
-    list_of_video += clip
+    list_of_video.append(clip)
 
 final_clip = concatenate_videoclips(list_of_video)
 final_clip.write_videofile("new.mp4")
